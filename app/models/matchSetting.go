d3253515308e430cb5167f25ea140fca2dbcea12
ayato yamauchi <>
2023-01-29 12:35:54 +0900

    change json変換時のために構造化タグを追加
    https://qiita.com/aibou/items/33142a988e8bd4a4d164
    複数タグ

--- app/models/matchSetting.go
+++ app/models/matchSetting.go
@@ -3,19 +3,19 @@ package models
 import "time"
 
 type MatchSetting struct {
-	RoomId           string    `firestore:"roomId"`
-	MahjongNumber    string    `firestore:"mahjongNumber"`
-	Uma              string    `firestore:"uma"`
-	Oka              int       `firestore:"oka"`
-	IsYakitori       bool      `firestore:"isYakitori"`
-	IsTobishou       bool      `firestore:"isTobishou"`
-	TobishouPoint    int       `firestore:"tobishouPoint"`
-	Rate             int       `firestore:"rate"`
-	IsTip            bool      `firestore:"isTip"`
-	TipInitialNumber int       `firestore:"tipInitialNumber"`
-	TipRate          int       `firestore:"tipRate"`
-	CreateTimestamp  time.Time `firestore:"createTimestamp"`
-	UpdateTimestamp  time.Time `firestore:"updateTimestamp"`
+	RoomId           string    `firestore:"roomId" json:"roomId"`
+	MahjongNumber    string    `firestore:"mahjongNumber" json:"mahjongNumber"`
+	Uma              string    `firestore:"uma" json:"uma"`
+	Oka              int       `firestore:"oka" json:"oka"`
+	IsYakitori       bool      `firestore:"isYakitori" json:"isYakitori"`
+	IsTobishou       bool      `firestore:"isTobishou" json:"isTobishou"`
+	TobishouPoint    int       `firestore:"tobishouPoint" json:"tobishouPoint"`
+	Rate             int       `firestore:"rate" json:"rate"`
+	IsTip            bool      `firestore:"isTip" json:"isTip"`
+	TipInitialNumber int       `firestore:"tipInitialNumber" json:"tipInitialNumber"`
+	TipRate          int       `firestore:"tipRate" json:"tipRate"`
+	CreateTimestamp  time.Time `firestore:"createTimestamp" json:"createTimestamp"`
+	UpdateTimestamp  time.Time `firestore:"updateTimestamp" json:"updateTimestamp"`
 }
 
 // type RespMatchSettingModel struct {
