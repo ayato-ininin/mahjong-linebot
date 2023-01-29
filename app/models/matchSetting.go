55e438af0c9c727dab65009e142107827c38e213
ayato yamauchi <>
2023-01-29 21:11:27 +0900

    add 名前も設定に追加

--- app/models/matchSetting.go
+++ app/models/matchSetting.go
@@ -5,6 +5,10 @@ import "time"
 type MatchSetting struct {
 	RoomId           string    `firestore:"roomId" json:"roomId"`
 	MahjongNumber    string    `firestore:"mahjongNumber" json:"mahjongNumber"`
+	Name1            string    `firestore:"name1" json:"name1"`
+	Name2            string    `firestore:"name2" json:"name2"`
+	Name3            string    `firestore:"name3" json:"name3"`
+	Name4            string    `firestore:"name4" json:"name4"`
 	Uma              string    `firestore:"uma" json:"uma"`
 	Oka              int       `firestore:"oka" json:"oka"`
 	IsYakitori       bool      `firestore:"isYakitori" json:"isYakitori"`
