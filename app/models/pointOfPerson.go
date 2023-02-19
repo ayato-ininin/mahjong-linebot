7952b85bf3463cc738a58705d0cc9e6935490344
ayato yamauchi <>
2023-02-19 14:43:55 +0900

    add プロパティ追加

--- app/models/pointOfPerson.go
+++ app/models/pointOfPerson.go
@@ -4,4 +4,5 @@ type PointOfPerson struct {
 	NameIndex  int64 `firestore:"nameIndex" json:"nameIndex"`
 	Point      int64 `firestore:"point" json:"point"`
 	IsYakitori bool  `firestore:"isYakiroti" json:"isYakiroti"`
+	IsTobishou bool  `firestore:"isTobishou" json:"isTobishou"`
 }
