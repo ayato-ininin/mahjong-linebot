4b096db03e9d9a0f49ce5b378dfee3a8ccfcf0e1
ayato yamauchi <>
2023-02-25 11:38:39 +0900

    コメント追加(共通処理したいが)

--- app/controllers/matchResult.go
+++ app/controllers/matchResult.go
@@ -145,3 +145,54 @@ func OptionsMatchResultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.WriteHeader(http.StatusOK)
 }
+
+// func PostMatchResult(w http.ResponseWriter, r *http.Request) {
+// 	post(w, r, firestore.AddMatchResult)
+// }
+
+// func UpdateMatchResult(w http.ResponseWriter, r *http.Request) {
+// 	traceID := logger.GetTraceId(r)
+// 	// contextを作成しtraceIdをセットする(リクエストを渡すのではなく、contextにしてfirestoreに渡す。traceIdにて追跡のため)
+// 	ctx := context.WithValue(r.Context(), "traceId", traceID)//r.Context()でリクエストのcontextを再利用
+// 	log.Printf(logger.InfoLogEntry(traceID, "UPDATE:MATCHRESULT START ==========="))
+// 	post(ctx, w, r, traceID, firestore.UpdateMatchResult)
+// 	log.Printf(logger.InfoLogEntry(traceID, "UPDATE:MATCHRESULT END ==========="))
+// }
+
+// /***
+// 	firebaseへ追加、更新処理の共通処理
+// 	interface{}はswtich等で型を判定しないといけないかも
+//  ***/
+// func post(ctx context.Context, w http.ResponseWriter, r *http.Request, traceID string, f func(context.Context, interface{}, time.Time) error) {
+// 	var m interface{}
+// 	err := json.NewDecoder(r.Body).Decode(&m)
+// 	if err != nil {
+// 			log.Printf(logger.ErrorLogEntry(traceID, "Failed json unmarshal", err))
+// 			utils.APIError(w, "Failed json unmarshal", http.StatusInternalServerError)
+// 			return
+// 	}
+// 	defer r.Body.Close()
+
+// 	jst := time.FixedZone("JST", 9*60*60)
+// 	err = f(ctx, &m, time.Now().In(jst))
+// 	if err != nil {
+// 			log.Printf(logger.ErrorLogEntry(traceID, "Failed", err))
+// 			utils.APIError(w, "Failed", http.StatusInternalServerError)
+// 			return
+// 	}
+
+// 	jsonData, err := json.Marshal(&m)
+// 	if err != nil {
+// 			log.Printf(logger.ErrorLogEntry(traceID, "Failed json marshal", err))
+// 			utils.APIError(w, "Failed json marshal", http.StatusInternalServerError)
+// 			return
+// 	}
+// 	log.Printf(logger.InfoLogEntry(traceID, "追加データ : %s", jsonData))
+
+// 	w.Header().Set("Content-Type", "application/json")
+// 	if err := json.NewEncoder(w).Encode(m); err != nil {
+// 			log.Printf(logger.ErrorLogEntry(traceID, "Failed json encode", err))
+// 			utils.APIError(w, "Failed json encode", http.StatusInternalServerError)
+// 			return
+// 	}
+// }
